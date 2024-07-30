d56f33041db003ba0e5e55d7de121464b9aae70b
samuelvandeven <samuel.vandeven@gmail.com>
2024-07-30 23:01:10 +0100

    console logs

--- WinmaxShopify15minSync/CallEndpointFunction.cs
+++ WinmaxShopify15minSync/CallEndpointFunction.cs
@@ -14,13 +14,14 @@ namespace WinmaxShopify15minSync
         public static async Task Run([TimerTrigger("0 */15 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"Shopify Winmax Timer trigger function executed at: {DateTime.Now}");
-
+            Console.WriteLine("Shopify winmax timer trigger function started.");
             try
             {
                 var response = await httpClient.GetAsync("https://rew03i8d.a2hosted.com/public/apply-mapping");
                 response.EnsureSuccessStatusCode();
 
                 log.LogInformation("Endpoint called successfully.");
+                Console.WriteLine("Endpoint called successfully.");
             }
             catch (Exception ex)
             {
